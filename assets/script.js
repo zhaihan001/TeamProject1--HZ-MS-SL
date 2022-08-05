b0f8e5dd03a5991d8cffcde897f7e525d39d79d3
Han Zhai <zhaihan001@gmail.com>
2022-08-04 21:41:21 -0700

    latest fully functional code

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,74 @@
 var dogUrl = 'https://dog.ceo/api/breeds/image/random'
 var catUrl = 'https://api.thecatapi.com/v1/images/search?api_key="8e030fc9-e7ee-47ee-9687-d6df7bee3a67"'
+var dogScore = localStorage.getItem("dogScore");
+var catScore = localStorage.getItem("catScore");
+
 var dogDisplay = $('#dogDisplay');
 var dogBtn = $('#dogBtn');
+
 var catDisplay = $('#catDisplay');
 var catBtn = $('#catBtn');
-var imgBoth = document.getElementsByTagName("img");
 
 var dogVote = 0;
 var catVote = 0;
-var dogScore = 1;
-var catScore = 1;
+
 var messageEl = document.getElementById("messageLine");
 
-
-function clear() {
-    for (let i = 0; i < imgBoth.length; i++) {
-        imgBoth[i].remove();
-    }
+function init(){
+    document.getElementById("dogScore").innerHTML = localStorage.getItem("dogScore");
+    document.getElementById("catScore").innerHTML = localStorage.getItem("catScore");
+    dogApi();
+    catApi(); 
 }
 
 function dogApi() {
-    clear();
     fetch(dogUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            var dogImg = document.createElement('img');
-            dogImg.src = data.message;
-            dogDisplay.prepend(dogImg);
-            console.log(data);
-            console.log(data.message);
+            document.getElementById("dogImg").src = data.message;
         })
 }
 
 function catApi() {
-    clear();
     fetch(catUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            var catImg = document.createElement('img');
-            catImg.src = data[0].url;
-            catDisplay.prepend(catImg);
-            console.log(data);
+            document.getElementById("catImg").src =  data[0].url;
         })
 }
 
-function voteCount() {
+function voteCount(event) {
 
     var voteItem = event.target.dataset.vote;
-    console.log(voteItem);
 
     if (voteItem == "dog") {
         dogVote++;
+        dogScore++;
         document.getElementById("dogProgress").value = dogVote;
-        localStorage.setItem("dogScore",dogScore++);
+        localStorage.setItem("dogScore",dogScore);
         document.getElementById("dogScore").innerHTML = localStorage.getItem("dogScore");
     }
-    else {
+    else{
         catVote++;
+        catScore++;
         document.getElementById("catProgress").value = catVote;
-        localStorage.setItem("catScore",catScore++);
+        localStorage.setItem("catScore",catScore);
         document.getElementById("catScore").innerHTML = localStorage.getItem("catScore");
 
     }
 
-    if (dogVote == 2) {
+    if (dogVote == 5) {
         messageEl.textContent = "Woof Woof! You must be a dog person!"
     }
-    else if (catVote == 2) {
+    else if (catVote == 5) {
         messageEl.textContent = "Meow! You must be a cat person!"
     }
 
-    if (dogVote == 3 || catVote == 3) {
+    if (dogVote == 6 || catVote == 6) {
         messageEl.textContent = ""
         dogVote = 0;
         catVote = 0;
@@ -83,21 +77,23 @@ function voteCount() {
     }
 }
 
+init();
+
 $('.voteBtn').on('click', function (event) {
     catApi();
     dogApi();
-    voteCount();
+    voteCount(event);
 })
 
 $('#resetBtn').on('click', function () {
     localStorage.clear();
-    document.getElementById("dogScore").innerHTML = "";
-    document.getElementById("catScore").innerHTML = "";
+    document.getElementById("dogScore").innerHTML = 0;
+    document.getElementById("catScore").innerHTML = 0;
     messageEl.textContent = ""
     dogVote = 0;
     catVote = 0;
     document.getElementById("dogProgress").value = dogVote;
     document.getElementById("catProgress").value = catVote;
-    dogScore = 1;
-    catScore = 1;
+    dogScore = 0;
+    catScore = 0;
 })
